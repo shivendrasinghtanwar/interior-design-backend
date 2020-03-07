844024fc59c8ae7133d813c2e4c4e34a0761b310
jatingarg7165 <40426961+jatingarg7165@users.noreply.github.com>
2020-03-07 13:20:53 +0530

    Update urlByRoles.js

--- config/constants/urlByRoles.js
+++ config/constants/urlByRoles.js
@@ -1,7 +1,7 @@
 const routeConstants = {
   adminUrls: [
     '/login', '/ADMIN', '/generate-design-quotation', '/add-client', '/fetch-all-client', '/pre-sales/fetch-all-unassigned-client',
-    '/pre-sales/assigned-client', '/assign-to-client'
+    '/pre-sales/assigned-client', '/assign-to-client', '/designer/assigned-client', '/designer/client-met', '/designer/update-client-met'
   ],
   userUrls: [
     '/check-client-req-form', '/fill-client-req-form'
