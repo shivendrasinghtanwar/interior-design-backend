e88402fc82c4c206a0ad09c73e34dd964896e34f
Shivendra Singh Tanwar <sunnysinghtanwar2@gmail.com>
2020-04-10 00:11:05 +0530

    Update all onsite record api to return boolean string instead of 0 and 1

--- src/models/boqQueries.js
+++ src/models/boqQueries.js
@@ -1,7 +1,17 @@
 class Queries {
   allOnSiteRecords() {
     return `select
-    *
+    id,
+    item_type,
+    item_description,
+    unit,
+    rate,
+    CASE nos WHEN '1' THEN 'true' ELSE 'false' END as nos,
+    CASE length WHEN '1' THEN 'true' ELSE 'false' END as length,
+    CASE height WHEN '1' THEN 'true' ELSE 'false' END as height,
+    CASE width WHEN '1' THEN 'true' ELSE 'false' END as width,
+    created,
+    updated
     from on_site_master_data;`;
   }
 
