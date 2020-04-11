a18648e6a266a2a7394118b47b6bda54d1e4cc47
Shivendra Singh Tanwar <sunnysinghtanwar2@gmail.com>
2020-04-11 22:36:41 +0530

    Add api to get - boq furniture records and boq furniture categories complete

--- src/models/boqQueries.js
+++ src/models/boqQueries.js
@@ -29,17 +29,27 @@ class Queries {
   allFurnitureRecords(category) {
     let queryString = `select
     id,
+    item_code,
     item_type,
+    item_name,
     item_description,
     unit,
     rate,
-    CASE nos WHEN '1' THEN 'true' ELSE 'false' END as nos,
-    CASE length WHEN '1' THEN 'true' ELSE 'false' END as length,
-    CASE height WHEN '1' THEN 'true' ELSE 'false' END as height,
-    CASE width WHEN '1' THEN 'true' ELSE 'false' END as width,
-    created,
-    updated
-    from on_site_master_data`;
+    amount,
+    piece_quantity,
+    breadth,
+    length,
+    height,
+    main_rate,
+    fabric_m,
+    fabric_rate,
+    drawer_count,
+    drawer_rate,
+    sliding,
+    sliding_rate,
+    url
+    width
+    from boq_furniture_master_data`;
     if (category) {
       queryString += ` where item_type='${category}'`;
     }
@@ -49,7 +59,7 @@ class Queries {
 
   allFurnitureCategories() {
     return `select distinct item_type as category
-    from on_site_master_data
+    from boq_furniture_master_data
     group by item_type`;
   }
 
