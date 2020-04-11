30f0fcca685a8ec31c32a85be36cde3c713e67ba
Shivendra Singh Tanwar <sunnysinghtanwar2@gmail.com>
2020-04-11 22:40:48 +0530

    Add api to get boq modular records and boq modular categories

--- src/models/boqQueries.js
+++ src/models/boqQueries.js
@@ -66,17 +66,27 @@ class Queries {
   allModularRecords(category) {
     let queryString = `select
     id,
+    item_code,
     item_type,
+    item_name,
     item_description,
     unit,
     rate,
-    CASE nos WHEN '1' THEN 'true' ELSE 'false' END as nos,
-    CASE length WHEN '1' THEN 'true' ELSE 'false' END as length,
-    CASE height WHEN '1' THEN 'true' ELSE 'false' END as height,
-    CASE width WHEN '1' THEN 'true' ELSE 'false' END as width,
-    created,
-    updated
-    from on_site_master_data`;
+    amount,
+    piece_quantity,
+    breadth,
+    length,
+    height,
+    main_rate,
+    fabric_m,
+    fabric_rate,
+    drawer_count,
+    drawer_rate,
+    sliding,
+    sliding_rate,
+    url
+    width
+    from boq_modular_master_data`;
     if (category) {
       queryString += ` where item_type='${category}'`;
     }
@@ -86,7 +96,7 @@ class Queries {
 
   allModularCategories() {
     return `select distinct item_type as category
-    from on_site_master_data
+    from boq_modular_master_data
     group by item_type`;
   }
 
